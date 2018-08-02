ae2f402b6e26e77ef80076bfc6d1d3229faf6110
ridouchire <ridouchire@gmail.com>
2018-08-02 23:19:40 +0400

    Influence of the amount of ammunition was added.

--- gun.ino
+++ gun.ino
@@ -3,17 +3,20 @@
 int anticheat = 6;
 int trigger = 17;
 int trasmitter = 14;
+int weapons;
 
 void setup()
 {
   pinMode(anticheat, OUTPUT);
   Serial.begin(9600);
+  weapons = 7;
 }
 
 void loop()
 {
-  if (digitalRead(trigger) == HIGH) {
+  if (digitalRead(trigger) == HIGH && weapons >= 0) {
     digitalWrite(trasmitter, HIGH);
+    --weapons;
     delay(50);
   } else {
     digitalWrite(trasmitter, LOW);
